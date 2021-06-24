2253e8f0c0a4511ae70db3868ec92e8eed9fa474
wendyrvllr <wendyrevailler@yahoo.fr>
2021-06-24 14:44:29 +0200

    add image resize

--- app/gaelo_processing/models/Inferences/InferencePTSegmentation.py
+++ app/gaelo_processing/models/Inferences/InferencePTSegmentation.py
@@ -1,7 +1,9 @@
 import SimpleITK as sitk
+from SimpleITK.SimpleITK import ImageReaderBase, Transform
+from SimpleITK.extra import GetImageFromArray
 import numpy as np
 from numpy.core.arrayprint import array2string
-from numpy.core.fromnumeric import resize
+from numpy.core.fromnumeric import reshape, resize
 import tensorflow as tf
 
 from django.conf import settings
@@ -20,11 +22,20 @@ class InferencePTSegmentation(AbstractInference):
         path_pt=data_path+'/image/image_'+idPT+'_PT.nii'
         img_ct=sitk.ReadImage(path_ct)
         img_pt=sitk.ReadImage(path_pt)
+
+        #save original direction, spacing and origin
+        self.spacing=img_pt.GetSpacing()
+        self.direction=img_pt.GetDirection()
+        self.origin=img_pt.GetOrigin()
+        self.size=img_pt.GetSize()
         fusion_object=Fusion()
         fusion_object.set_origin_image(img_pt)
         fusion_object.set_target_volume((128,128,256),(4.0, 4.0, 4.0),(1,0,0,0,1,0,0,0,1))
         ct_resampled = fusion_object.resample(img_ct,-1000.0)
         pt_resampled = fusion_object.resample(img_pt,0)
+        self.pt_resampled_origin = pt_resampled.GetOrigin()
+        self.pt_resampled_spacing = pt_resampled.GetSpacing()
+        self.pt_resampled_direction = pt_resampled.GetDirection()
 
         ct_array = sitk.GetArrayFromImage(ct_resampled)
         pt_array = sitk.GetArrayFromImage(pt_resampled)
@@ -46,12 +57,23 @@ class InferencePTSegmentation(AbstractInference):
         results=result.outputs['tf.math.sigmoid_4']
         shape=tf.TensorShape(results.tensor_shape)
         array = np.array(results.float_val).reshape(shape.as_list())
-        array=np.around(array)
-        print(array.shape)
-        data_path = settings.STORAGE_DIR
-        image = sitk.GetImageFromArray(array, True)
-        sitk.WriteImage(image, data_path+'/image/image_from_array.nii')    
+        array=np.around(array).astype(np.int16)
+        image = sitk.GetImageFromArray(array[0,:,:,:,0])
         
+        image.SetDirection(self.pt_resampled_direction)
+        image.SetOrigin(self.pt_resampled_origin)
+        image.SetSpacing(self.pt_resampled_spacing)
+        
+        transformation = sitk.ResampleImageFilter()        
+        transformation.SetOutputDirection(self.direction)
+        transformation.SetOutputOrigin(self.origin)
+        transformation.SetOutputSpacing(self.spacing)
+        transformation.SetSize(self.size)
+        transformation.SetDefaultPixelValue(0.0)
+        transformation.SetInterpolator(sitk.sitkNearestNeighbor)
+        image=transformation.Execute(image)
+        data_path = settings.STORAGE_DIR
+        sitk.WriteImage(image, data_path+'/image/image_from_array.nii')
         #return result
 
     def get_input_name(self) -> str:
@@ -59,3 +81,7 @@ class InferencePTSegmentation(AbstractInference):
     
     def get_model_name(self) -> str:
         return 'pt_segmentation_model'
+
+    def __save_to_nifti(image):
+        data_path = settings.STORAGE_DIR
+        sitk.WriteImage(image, data_path+'/image/image_from_array.nii')
