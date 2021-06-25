9121d75afb6e7c89e0fc6b1a1c29e758ae7e5b63
Nicolas <nicolas.jacomi@etud.univ-jfc.fr>
2021-06-25 09:17:01 +0200

    up test dicom to cnn

--- app/tests/unitaire/test_dicom_to_cnn.py
+++ app/tests/unitaire/test_dicom_to_cnn.py
@@ -1,15 +1,17 @@
 from django.test import TestCase
+from django.conf import settings
 
 from ...gaelo_processing.data_transform.dicom_to_nifti import DicomToCnn
 
 class test_dicom_to_cnn(TestCase):
-    # def test_to_nifti(self):
-    #     DicomToCnn.to_nifti(self,'C:/Users/Nicolas/AppData/Local/Temp/gaelo_pross_unzip_jz60e_9p')
+    def test_to_nifti(self):
+        data_path = settings.STORAGE_DIR
+        DicomToCnn.to_nifti(self,data_path+'/dicom/1.2.276.0.7230010.3.1.4.2267612261.1368.1197888473.4/1.2.276.0.7230010.3.1.4.2267612261.1368.1197888473.5/7030106071217114750')
 
     # def test_generate_mip(self):
     #     DicomToCnn.generate_mip(self,'9d1f6b1606e0b318a8dff0928564ccc4')
     #     print('test dicom-to-cnn.generate_mip validate')
 
-    def test_fusion(self):
-        DicomToCnn.fusion(self,'1.3.12.2.1107.5.1.4.45520.30000013102404450495300000013')
-        print('test dicom-to-cnn.fusion validate')
+    # def test_fusion(self):
+    #     DicomToCnn.fusion(self,'1.3.12.2.1107.5.1.4.45520.30000013102404450495300000013')
+    #     print('test dicom-to-cnn.fusion validate')
