9965fef22d6b9e0e4f1d00c19bdc57a99430dcfe
wendyrvllr <wendyrevailler@yahoo.fr>
2021-07-07 13:00:16 +0200

    up utips

--- app/gaelo_processing/utips/utips.py
+++ app/gaelo_processing/utips/utips.py
@@ -1,6 +1,9 @@
 import tempfile
 import os
 import shutil
+import hashlib
+
+from django.conf import settings
 
 from zipfile import ZipFile
 
@@ -15,7 +18,11 @@ class Utips:
         Returns:
             NONE
         """
-        destination = tempfile.mkdtemp(prefix='gaelo_pross_unzip_')
+       
+        image_md5 = hashlib.md5(str(zip_file).encode())
+        dicom_id = image_md5.hexdigest()
+        os.mkdir(settings.STORAGE_DIR+'/dicom/dicom_serie_'+dicom_id)
+        destination=settings.STORAGE_DIR+'/dicom/dicom_serie_'+dicom_id
         with ZipFile(zip_file) as my_zip:
             for member in my_zip.namelist():
                 filename = os.path.basename(member)
@@ -24,7 +31,9 @@ class Utips:
                     continue
                 # copy file (taken from zipfile's extract)
                 source = my_zip.open(member)
+                
                 target = open(os.path.join(destination, filename), "wb")
                 with source, target:
                     shutil.copyfileobj(source, target)
-            # return destination
+                dict={'id_dicom_serie':dicom_id}
+            return dict
